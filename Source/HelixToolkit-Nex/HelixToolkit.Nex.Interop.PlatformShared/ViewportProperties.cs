d9d7e0b43f66ec587944b6166f2d9d73d9c1d1aa
holance <holance.app@gmail.com>
2026-04-16 10:16:00 -0700

    Move common viewport properties into shared project.

---
+++ Source/HelixToolkit-Nex/HelixToolkit.Nex.Interop.PlatformShared/ViewportProperties.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HelixToolkit.Nex.Engine.CameraControllers;
+using HelixToolkit.Nex.Interop;
+#if HxWPF
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace HelixToolkit.Nex.Wpf;
+
+#elif HxWinUI
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace HelixToolkit.Nex.WinUI;
+
+#else
+#error Unknown framework
+#endif
+
+public partial class HelixViewport
+{
+    #region Dependency Properties
+    public static readonly DependencyProperty EngineDp = HelixProperty.Register<
+        HelixViewport,
+        Engine.Engine?
+    >(
+        "Engine",
+        null,
+        (d, e) =>
+        {
+            if (d is not HelixViewport viewport)
+            {
+                return;
+            }
+            viewport.SetEngine((Engine.Engine?)e.NewValue);
+        }
+    );
+    public Engine.Engine? Engine
+    {
+        get { return (Engine.Engine)GetValue(EngineDp); }
+        set { SetValue(EngineDp, value); }
+    }
+
+    public static readonly DependencyProperty ViewportClientDp = HelixProperty.Register<
+        HelixViewport,
+        IViewportClient?
+    >(
+        "ViewportClient",
+        null,
+        (d, e) =>
+        {
+            if (d is not HelixViewport viewport)
+            {
+                return;
+            }
+            viewport.SetClient((IViewportClient?)e.NewValue);
+        }
+    );
+
+    /// <summary>
+    /// Gets or sets the <see cref="IViewportClient"/> that provides per-frame camera
+    /// updates and scene data for this viewport. When <c>null</c>, no frames are rendered.
+    /// </summary>
+    public IViewportClient? ViewportClient
+    {
+        get { return (IViewportClient?)GetValue(ViewportClientDp); }
+        set { SetValue(ViewportClientDp, value); }
+    }
+    public static readonly DependencyProperty CameraControllerDp = HelixProperty.Register<
+        HelixViewport,
+        ICameraController?
+    >(
+        "CameraController",
+        null,
+        (d, e) =>
+        {
+            if (d is not HelixViewport viewport)
+            {
+                return;
+            }
+            viewport.SetCameraController((ICameraController?)e.NewValue);
+        }
+    );
+
+    /// <summary>
+    /// Gets or sets the <see cref="ICameraController"/> that handles user input to manipulate the camera.
+    /// </summary>
+    public ICameraController? CameraController
+    {
+        get { return (ICameraController?)GetValue(ViewportClientDp); }
+        set { SetValue(ViewportClientDp, value); }
+    }
+    #endregion
+}
